af4e10e7162c0a459c3cc49bd62c7598ec484cc9
dimits-exe <70580393+dimits-exe@users.noreply.github.com>
2021-12-21 22:26:47 +0200

    Added brief description

--- src/entity/package-info.java
+++ src/entity/package-info.java
@@ -1,7 +1,9 @@
 /**
- * TODO
+ * The entity package contains the application's Entity classes as
+ * well as other record files used by them.
  *
  *
  * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
  */
 package entity;
