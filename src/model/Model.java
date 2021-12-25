0cc7b0d5232bccd62e55e70e50f4ca0a4311b4bd
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-12-25 22:46:21 +0200

    Add Model.java
    An implementation of the IModel interface

---
+++ src/model/Model.java
@@ -0,0 +1,147 @@
+package model;
+
+import java.awt.image.BufferedImage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import entity.ELine;
+import entity.EStation;
+import entity.ETimetable;
+import entity.ETown;
+import entity.LineType;
+import entity.MissingSpriteException;
+import requirement.util.Requirements;
+
+/**
+ * An implementation of the {@link IModel} interface.
+ *
+ * @author Alex Mandelias
+ */
+public class Model implements IModel {
+
+	private static final String URL  = "jdbc:mysql://localhost/oasaripoff"; //$NON-NLS-1$
+	private static final String USER = "root";                              //$NON-NLS-1$
+	private static final String PASS = "localhostMVCMy$QL";                 //$NON-NLS-1$
+
+	@FunctionalInterface
+	private static interface SQLExecutable<S extends Statement, R> {
+		public R execute(S statement) throws SQLException;
+	}
+
+	private static <R> R executeStatement(SQLExecutable<Statement, R> executable) throws SQLException {
+		try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+		        Statement stmt = conn.createStatement();) {
+			return executable.execute(stmt);
+		}
+	}
+
+	private static <R> R executePrepared(String sql, SQLExecutable<PreparedStatement, R> executable)
+	        throws SQLException {
+		try (Connection conn = DriverManager.getConnection(URL, USER, PASS);
+		        PreparedStatement stmt = conn.prepareStatement(sql);) {
+			return executable.execute(stmt);
+		}
+	}
+
+	/**
+	 * Constructs a Model that communicates with the underlying database to retrieve
+	 * data and return them using Entity objects from the {@link entity} package.
+	 */
+	public Model() {}
+
+	@Override
+	public List<ETown> getTowns(Requirements reqs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+
+		/*
+		Example use of the private static methods and interface:
+
+		@foff
+		return executePrepared("SELECT * FROM City WHERE name REGEXP=?",
+		        (PreparedStatement s) -> {
+
+		            s.setString(0, reqs.getValue("regex", String.class));
+			        List<ETown> towns = new LinkedList<>();
+			        try (ResultSet rs = s.executeQuery()) {
+				        while (rs.next()) {
+					        towns.add(new ETown(rs.getString("name")));
+				        }
+			        }
+			        return towns;
+		});
+		@on
+		*/
+	}
+
+	@Override
+	public List<ELine> getLines(Requirements reqs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<EStation> getStations(Requirements reqs) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BufferedImage getVehicleSprite(LineType type) throws MissingSpriteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertLine(ELine line) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertTown(ETown town) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertStation(EStation station) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void insertTimetable(ELine line, ETimetable timetable) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteTown(ETown town) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteStation(EStation station) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteLine(ELine line, ETimetable timetable) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteTimeTable(ELine line, ETimetable timetable) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
