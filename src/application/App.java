368ef8fe427e01b1837a22b55ea1dc5952a27199
AAAlex-123 <60742680+AAAlex-123@users.noreply.github.com>
2021-12-25 22:31:35 +0200

    Add application package
    It contains the `App` class which runs the application

---
+++ src/application/App.java
@@ -0,0 +1,33 @@
+package application;
+
+import controller.Controller;
+import controller.IController;
+import model.IModel;
+import model.Model;
+import view.IView;
+import view.View;
+
+/**
+ * The entry point of the program. Constructs and runs an {@link view.IView
+ * View}.
+ *
+ * @author Alex Mandelias
+ */
+public class App {
+
+	/**
+	 * Constructs and runs an Application.
+	 *
+	 * @param args command line arguments (not used)
+	 */
+	public static void main(String[] args) {
+
+		IModel      model      = new Model();
+		IView       view       = new View();
+		IController controller = new Controller(model, view);
+
+		view.registerController(controller);
+
+		view.start();
+	}
+}
