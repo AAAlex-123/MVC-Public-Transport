7688536ce8a021a16413b50f77b584615a8f02c2
Alex Mandelias <alexmandelias@gmail.com>
2021-12-31 03:03:53 +0200

    improve sql create file

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,5 @@
-CREATE DATABASE IF NOT EXISTS oasaripoff;
+DROP DATABASE IF EXISTS oasaripoff;
+CREATE DATABASE oasaripoff;
 
 USE oasaripoff;
 
@@ -18,29 +19,36 @@ CREATE TABLE IF NOT EXISTS Station (
 
 CREATE TABLE IF NOT EXISTS Line (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255),
+	lineNo VARCHAR(7), /* A5 / 308 / B9 */
+	description VARCHAR(255), /* start-end */
 	type VARCHAR(15),
-	is_return BOOLEAN
 );
 
 CREATE TABLE IF NOT EXISTS LineStation (
 	line_id INT,
 	station_id INT,
+	station_index INT, /* ! */
 	PRIMARY KEY (line_id, station_id),
 	FOREIGN KEY (line_id) REFERENCES Line(id),
 	FOREIGN KEY (station_id) REFERENCES Station(id)
 );
 
-CREATE TABLE IF NOT EXISTS Timetable (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	hour INT,
-	minute INT
-);
-
 CREATE TABLE IF NOT EXISTS LineTimetable (
 	line_id INT,
-	timetable_id INT,
-	PRIMARY KEY (line_id, timetable_id),
+	departure_time TIME, /* ! */
+	
+	PRIMARY KEY (line_id, departure_time),
 	FOREIGN KEY (line_id) REFERENCES Line(id),
-	FOREIGN KEY (timetable_id) REFERENCES Timetable(id)
 );
+
+/*
+pp with:
+er
+diagramma klasewn
+activity diagram for 2-3 activities (PCE for VCM)
+very sinoptika o kwdikas
+
+leitoyrgikothta -> arxitektonikh
+er -> model ->
+
+*/
