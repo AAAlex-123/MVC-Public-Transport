c7c3a9e627d4bb1481a50e0831475076f47bc278
Alex Mandelias <alexmandelias@gmail.com>
2022-01-03 02:43:20 +0200

    clean up .sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -19,15 +19,15 @@ CREATE TABLE IF NOT EXISTS Station (
 
 CREATE TABLE IF NOT EXISTS Line (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	lineNo VARCHAR(7), /* A5 / 308 / B9 */
-	description VARCHAR(255), /* start-end */
-	type VARCHAR(15),
+	lineNo VARCHAR(7),
+	description VARCHAR(255),
+	type VARCHAR(15)
 );
 
 CREATE TABLE IF NOT EXISTS LineStation (
 	line_id INT,
 	station_id INT,
-	station_index INT, /* ! */
+	station_index INT,
 	PRIMARY KEY (line_id, station_id),
 	FOREIGN KEY (line_id) REFERENCES Line(id),
 	FOREIGN KEY (station_id) REFERENCES Station(id)
@@ -35,20 +35,7 @@ CREATE TABLE IF NOT EXISTS LineStation (
 
 CREATE TABLE IF NOT EXISTS LineTimetable (
 	line_id INT,
-	departure_time TIME, /* ! */
-	
+	departure_time TIME,
 	PRIMARY KEY (line_id, departure_time),
-	FOREIGN KEY (line_id) REFERENCES Line(id),
+	FOREIGN KEY (line_id) REFERENCES Line(id)
 );
-
-/*
-pp with:
-er
-diagramma klasewn
-activity diagram for 2-3 activities (PCE for VCM)
-very sinoptika o kwdikas
-
-leitoyrgikothta -> arxitektonikh
-er -> model ->
-
-*/
