8b72bd18a64cbc588599417b1cc429497c4575c9
Alex Mandelias <alexmandelias@gmail.com>
2022-01-03 03:24:37 +0200

    add test view and class

---
+++ src/view/TestView.java
@@ -0,0 +1,46 @@
+package view;
+
+import java.util.List;
+
+import entity.ELine;
+import entity.EStation;
+import entity.ETimetable;
+import entity.ETown;
+
+/**
+ * TODO
+ *
+ *
+ * @author Alex Mandelias
+ */
+public class TestView extends AbstractView {
+
+	public TestView() {
+		super(null);
+	}
+
+	@Override
+	public void updateViewWithTowns(List<ETown> towns) {
+		System.out.println(towns);
+	}
+
+	@Override
+	public void updateViewWithLines(List<ELine> lines) {
+		System.out.println(lines);
+	}
+
+	@Override
+	public void updateViewWithStations(List<EStation> stations) {
+		System.out.println(stations);
+	}
+
+	@Override
+	public void updateViewWithTimetables(List<ETimetable> timetables) {
+		System.out.println(timetables);
+	}
+
+	@Override
+	public void updateViewWithError(Exception e) {
+		System.out.println(e);
+	}
+}
