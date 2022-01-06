7d573969a6b4d1fb34f8f5a12cdeacf69402e790
Alex Mandelias <alexmandelias@gmail.com>
2022-01-07 01:57:26 +0200

    add IImageController and implementation

---
+++ src/controller/IImageController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import java.awt.Image;
+
+import model.IImageModel;
+
+/**
+ * An interface for the {@code Controller} responsible for loading images from
+ * the disk. This controller communicates with the {@link model.IImageModel} to
+ * scale and return the requested Images.
+ *
+ * @author Alex Mandelias
+ * @author Dimitris Tsirmpas
+ */
+public interface IImageController {
+
+	/**
+	 * Loads an image from the {@link IImageModel} and resizes it if necessary.
+	 *
+	 * @param name      the image's name
+	 * @param maxWidth  the maximum width of the image
+	 * @param maxHeight the maximum height of the image
+	 *
+	 * @return a possibly resized, loaded Image
+	 */
+	Image loadImage(String name, int maxWidth, int maxHeight);
+}
