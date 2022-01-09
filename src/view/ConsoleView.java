d5b2fbeb19eb9744d8342b3cd4f9860e966ccd8e
Alex Mandelias <alexmandelias@gmail.com>
2022-01-09 17:46:35 +0200

    externalise strings of the ConsoleView class

--- src/view/ConsoleView.java
+++ src/view/ConsoleView.java
@@ -1,5 +1,21 @@
 package view;
 
+import static localisation.ConsoleViewStrings.A_LINE;
+import static localisation.ConsoleViewStrings.A_STATION;
+import static localisation.ConsoleViewStrings.A_TOWN;
+import static localisation.ConsoleViewStrings.EMPTY;
+import static localisation.ConsoleViewStrings.LINES;
+import static localisation.ConsoleViewStrings.PLEASE_SELECT;
+import static localisation.ConsoleViewStrings.RETURN_TO_HOME;
+import static localisation.ConsoleViewStrings.SELECT_ACCESS;
+import static localisation.ConsoleViewStrings.SHOW_ALL;
+import static localisation.ConsoleViewStrings.STATIONS;
+import static localisation.ConsoleViewStrings.THE_LINE;
+import static localisation.ConsoleViewStrings.THE_STATION;
+import static localisation.ConsoleViewStrings.THE_TOWN;
+import static localisation.ConsoleViewStrings.TIMETABLES;
+import static localisation.ConsoleViewStrings.TOWNS;
+
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.List;
@@ -9,6 +25,7 @@ import entity.ELine;
 import entity.EStation;
 import entity.ETimetable;
 import entity.ETown;
+import localisation.Languages;
 import requirement.requirements.AbstractRequirement;
 import requirement.requirements.ListRequirement;
 import requirement.requirements.StringRequirement;
@@ -22,13 +39,12 @@ import requirement.util.Requirements;
  * @author Alex Mandelias
  * @author Dimitris Tsirmpas
  */
-@SuppressWarnings("nls")
 public class ConsoleView extends AbstractView<String> {
 
 	private final Scanner     in;
 	private final PrintStream out, err;
 
-	private String sourceEntityDescription = "";
+	private String sourceEntityDescription = EMPTY;
 
 	public ConsoleView() {
 		this(System.in, System.out, System.err);
@@ -43,7 +59,7 @@ public class ConsoleView extends AbstractView<String> {
 
 	@Override
 	public void start() {
-		out.println("Welcome! Please select one of the options below:");
+		out.println(Languages.getString("ConsoleView.0")); //$NON-NLS-1$
 		updateViewWithHomepage();
 	}
 
@@ -54,131 +70,128 @@ public class ConsoleView extends AbstractView<String> {
 
 	@Override
 	public void updateViewWithHomepage() {
-		sourceEntityDescription = "";
+		sourceEntityDescription = EMPTY;
 
 		printMenu(new String[] {
-		        "Show all Lines",
-		        "Show all Towns",
-		        "Insert data",
-		        "Exit",
-		}, "");
+		        String.format("%s %s", SHOW_ALL, LINES), //$NON-NLS-1$
+		        String.format("%s %s", SHOW_ALL, TOWNS), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.3"), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.4"), //$NON-NLS-1$
+		}, EMPTY);
 
 		doWithAnswer(
-		        () -> super.getAllLines(),
-		        () -> super.getAllTowns(),
-		        () -> showInsertMenu(),
+		        super::getAllLines,
+		        super::getAllTowns,
+		        this::showInsertMenu,
 		        () -> System.exit(0));
 	}
 
 	@Override
 	public void updateViewWithTowns(List<ETown> towns) {
-		if (towns.isEmpty()) {
-			goToHomepage("towns");
-		}
+		if (towns.isEmpty())
+			goToHomepage(TOWNS);
 
 		printSource();
 
-		FormatBuffer buffer = new FormatBuffer("Please select a town: \n");
+		final FormatBuffer buffer = new FormatBuffer("%s %s: %n", PLEASE_SELECT, //$NON-NLS-1$
+		        A_TOWN);
 
-		for (ETown town : towns) {
+		for (final ETown town : towns)
 			buffer.format(factory.getETownGraphic(town));
-		}
 
 		out.print(buffer);
 
-		ETown selected = towns.get(getAnswer(1, towns.size()) - 1);
+		final ETown selected = towns.get(getAnswer(1, towns.size()) - 1);
 
 		sourceEntityDescription = factory.getETownGraphic(selected);
 
 		printMenu(new String[] {
-		        "Show all lines going through this town",
-		        "Show all stations in this town",
-		        "Return to homepage",
-		},
-		        "Select what you want to access from the town: %s%n", selected.getName());
+		        String.format(Languages.getString("ConsoleView.6"), SHOW_ALL), //$NON-NLS-1$
+		        String.format(Languages.getString("ConsoleView.7"), SHOW_ALL), //$NON-NLS-1$
+		        RETURN_TO_HOME,
+		}, String.format("%s %s: %s%n", SELECT_ACCESS, //$NON-NLS-1$
+		        THE_TOWN), selected.getName());
 
 		doWithAnswer(() -> super.getLinesByTown(selected), () -> super.getStationsByTown(selected),
-		        () -> updateViewWithHomepage());
+		        this::updateViewWithHomepage);
 	}
 
 	@Override
 	public void updateViewWithLines(List<ELine> lines) {
-		if (lines.isEmpty()) {
-			goToHomepage("lines");
-		}
+		if (lines.isEmpty())
+			goToHomepage(LINES);
 
 		printSource();
-		FormatBuffer buffer = new FormatBuffer("Please select a line:\n");
+		final FormatBuffer buffer = new FormatBuffer("%s %s: %n", PLEASE_SELECT, //$NON-NLS-1$
+		        A_LINE);
 
-		for (ELine line : lines) {
+		for (final ELine line : lines)
 			buffer.format(factory.getELineGraphic(line));
-		}
 
 		out.print(buffer);
 
-		ELine selected = lines.get(getAnswer(1, lines.size()) - 1);
+		final ELine selected = lines.get(getAnswer(1, lines.size()) - 1);
 
 		sourceEntityDescription = factory.getELineGraphic(selected);
 
 		printMenu(new String[] {
-		        "Show all towns serviced by this line",
-		        "Show all stations serviced by this line",
-		        "Show all arrival times for this line",
-		        "Return to homepage",
-		}, "Select what you want to access from the line %s:%n", selected.getName());
+		        String.format(Languages.getString("ConsoleView.10"), SHOW_ALL), //$NON-NLS-1$
+		        String.format(Languages.getString("ConsoleView.11"), SHOW_ALL), //$NON-NLS-1$
+		        String.format(Languages.getString("ConsoleView.12"), SHOW_ALL), //$NON-NLS-1$
+		        RETURN_TO_HOME,
+		}, String.format("%s %s: %s%n", SELECT_ACCESS, //$NON-NLS-1$
+		        THE_LINE), selected.getName());
 
 		doWithAnswer(
 		        () -> super.getTownsByLine(selected),
 		        () -> super.getStationsByLine(selected),
 		        () -> super.getTimetablesByLine(selected),
-		        () -> updateViewWithHomepage());
+		        this::updateViewWithHomepage);
 	}
 
 	@Override
 	public void updateViewWithStations(List<EStation> stations) {
-		if (stations.isEmpty()) {
-			goToHomepage("stations");
-		}
+		if (stations.isEmpty())
+			goToHomepage(STATIONS);
 
 		printSource();
-		FormatBuffer buffer = new FormatBuffer("Please select a station:\n");
+		final FormatBuffer buffer = new FormatBuffer("%s %s: %n", PLEASE_SELECT, //$NON-NLS-1$
+		        A_STATION);
 
-		for (EStation station : stations) {
+		for (final EStation station : stations)
 			buffer.format(factory.getEStationGraphic(station));
-		}
 
 		out.print(buffer);
 
-		EStation selected = stations.get(getAnswer(1, stations.size()) - 1);
+		final EStation selected = stations.get(getAnswer(1, stations.size()) - 1);
 
 		sourceEntityDescription = factory.getEStationGraphic(selected);
 
 		printMenu(new String[] {
-		        "Show all lines going through this station",
-		        "Return to homepage",
-		}, "Select what you want to access from the station %s:%n", selected.getName());
+		        String.format(Languages.getString("ConsoleView.15"), SHOW_ALL), //$NON-NLS-1$
+		        RETURN_TO_HOME,
+		}, String.format("%s %s: %s%n", SELECT_ACCESS, //$NON-NLS-1$
+		        THE_STATION), selected.getName());
 
 		doWithAnswer(
 		        () -> super.getLinesByStation(selected),
-		        () -> updateViewWithHomepage());
+		        this::updateViewWithHomepage);
 	}
 
 	@Override
 	public void updateViewWithTimetables(List<ETimetable> timetables) {
-		if (timetables.isEmpty()) {
-			goToHomepage("timetables");
-		}
+		if (timetables.isEmpty())
+			goToHomepage(TIMETABLES);
 
 		printSource();
-		FormatBuffer buffer = new FormatBuffer();
+		final FormatBuffer buffer = new FormatBuffer();
 
-		for (ETimetable timetable : timetables) {
+		for (final ETimetable timetable : timetables)
 			buffer.format(factory.getETimetableGraphic(timetable));
-		}
 
 		out.print(buffer);
 
-		sourceEntityDescription = "";
+		sourceEntityDescription = EMPTY;
 
 		updateViewWithHomepage();
 	}
@@ -192,36 +205,34 @@ public class ConsoleView extends AbstractView<String> {
 	protected void fulfilRequirements(Requirements reqs, String prompt) {
 		out.println(System.lineSeparator() + prompt);
 
-		for (AbstractRequirement req : reqs) {
+		for (final AbstractRequirement req : reqs)
 			if (req instanceof ListRequirement) {
-				out.println("Please select one of the following options:");
+				out.println(Languages.getString("ConsoleView.17")); //$NON-NLS-1$
 
 				@SuppressWarnings("unchecked")
 				final List<Object> options = ((ListRequirement<Object>) req).getOptions();
 				final FormatBuffer buffer  = new FormatBuffer();
 
-				for (Object option : options)
+				for (final Object option : options)
 					buffer.format(option.toString());
 
 				out.print(buffer);
 
 				int answer = getAnswer(1, options.size()) - 1;
-				while (!req.finalise(options.get(answer))) {
+				while (!req.finalise(options.get(answer)))
 					answer = getAnswer(1, options.size()) - 1;
-					System.out.println("lmao can this even happen?");
-				}
+				// System.out.println("lmao can this even happen?");
 
 			} else if (req instanceof StringRequirement) {
 				final StringRequirement stringReq = (StringRequirement) req;
 				final String            key       = stringReq.key();
 
-				out.printf("Give a value for: %s%n", key);
+				out.printf(Languages.getString("ConsoleView.18"), key); //$NON-NLS-1$
 
 				while (!req.finalise(in.nextLine()))
-					err.printf("Invalid value for key '%s'. Please provide a(n): %s%n",
+					err.printf(Languages.getString("ConsoleView.19"), //$NON-NLS-1$
 					        stringReq.stringType.getDescription());
 			}
-		}
 	}
 
 	private static class FormatBuffer {
@@ -230,7 +241,7 @@ public class ConsoleView extends AbstractView<String> {
 		private int                count;
 
 		public FormatBuffer() {
-			this("");
+			this(EMPTY);
 		}
 
 		public FormatBuffer(String initial) {
@@ -238,8 +249,12 @@ public class ConsoleView extends AbstractView<String> {
 			count = 1;
 		}
 
+		public FormatBuffer(String format, Object... args) {
+			this(String.format(format, args));
+		}
+
 		public void format(String format, Object... args) {
-			final String preparedString = String.format("%d: %s%n", count, format);
+			final String preparedString = String.format("%d: %s%n", count, format); //$NON-NLS-1$
 			buffer.append(String.format(preparedString, args));
 			count++;
 		}
@@ -251,7 +266,7 @@ public class ConsoleView extends AbstractView<String> {
 	}
 
 	private void goToHomepage(String reason) {
-		out.printf("%nNo %s to show%n", reason);
+		out.printf(Languages.getString("ConsoleView.21"), reason); //$NON-NLS-1$
 		updateViewWithHomepage();
 	}
 
@@ -272,44 +287,43 @@ public class ConsoleView extends AbstractView<String> {
 		while (true) {
 			int answer;
 			try {
-				out.print("> ");
+				out.print("> "); //$NON-NLS-1$
 				answer = Integer.parseInt(in.nextLine());
-			} catch (NumberFormatException ne) {
+			} catch (final NumberFormatException ne) {
 				answer = bottomRange - 1;
 			}
 
 			if ((bottomRange <= answer) && (answer <= upperRange))
 				return answer;
 
-			err.printf("Please provide an answer between %d and %d%n%n",
+			err.printf(Languages.getString("ConsoleView.22"), //$NON-NLS-1$
 			        bottomRange, upperRange);
 		}
 	}
 
 	private void showInsertMenu() {
 		printMenu(new String[] {
-		        "Insert a new Line",
-		        "Insert a new Town",
-		        "Insert a new Station",
-		        "Insert an existing Station to a Line",
-		        "Insert a new Timetable to a Line",
-		        "Return to homepage",
-		}, "");
+		        Languages.getString("ConsoleView.23"), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.24"), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.25"), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.26"), //$NON-NLS-1$
+		        Languages.getString("ConsoleView.27"), //$NON-NLS-1$
+		        RETURN_TO_HOME,
+		}, EMPTY);
 
 		doWithAnswer(
-			() -> super.insertLine(),
-			() -> super.insertTown(),
-			() -> super.insertStation(),
-			() -> super.insertStationToLine(),
-			() -> super.insertTimetableToLine()
-		// ignore 6 - return to homepage, it always happens
-		);
+		        super::insertLine,
+		        super::insertTown,
+		        super::insertStation,
+		        super::insertStationToLine,
+		        super::insertTimetableToLine,
+		        this::updateViewWithHomepage);
 
 		updateViewWithHomepage();
 	}
 
 	private void printSource() {
 		if (!sourceEntityDescription.isEmpty())
-			out.printf("%nFrom %s:%n", sourceEntityDescription);
+			out.printf(Languages.getString("ConsoleView.28"), sourceEntityDescription); //$NON-NLS-1$
 	}
 }
