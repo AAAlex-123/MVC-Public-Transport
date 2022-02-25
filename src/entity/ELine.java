be197a5f2432b28d039e6cf60ebaaf3569c2e6b9
Alex Mandelias <alexmandelias@gmail.com>
2022-02-25 12:14:26 +0200

    add funtionality and tests for distance to station to ELine

--- src/entity/ELine.java
+++ src/entity/ELine.java
@@ -1,6 +1,7 @@
 package entity;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
@@ -8,8 +9,8 @@ import localisation.Languages;
 
 /**
  * The ELine class represents a public transport Line. It contains details about
- * the line as well as the {@link EStation Stations} and {@link ETimetable
- * timetables} associated with it.
+ * the line as well as the {@link EStation Stations} and {@link ETimestamp
+ * Departure Times} associated with it.
  *
  * @author Alex Mandelias
  * @author Dimitris Tsirmpas
@@ -20,7 +21,7 @@ public class ELine extends AbstractEntity {
 	private final LineType         type;
 	private final String           description;
 	private final List<EStation>   stations;
-	private final List<ETimetable> startTimes;
+	private final List<ETimestamp> startTimes;
 
 	/**
 	 * Constructs a Line.
@@ -33,7 +34,7 @@ public class ELine extends AbstractEntity {
 	 * @param startTimes  the list of Times when this Line departs
 	 */
 	public ELine(int id, String name, LineType type, String description,
-	        List<EStation> stations, List<ETimetable> startTimes) {
+	        List<EStation> stations, List<ETimestamp> startTimes) {
 		super(id);
 		this.name = name;
 		this.type = type;
@@ -46,7 +47,7 @@ public class ELine extends AbstractEntity {
 
 	/**
 	 * Returns this Line's name. This should be unique to each {@link LineType}. For
-	 * example, Subway line 3 is different than the Bus Line 3 despite having the
+	 * example, Subway line 3 is distinguishable from Bus Line 3 despite having the
 	 * same name.
 	 *
 	 * @return this Line's name
@@ -56,7 +57,7 @@ public class ELine extends AbstractEntity {
 	}
 
 	/**
-	 * Returns this Line's {@link LineType type,} the type of the public transport
+	 * Returns this Line's {@link LineType type}, the type of the public transport
 	 * vehicle operating on this Line.
 	 *
 	 * @return this Line's type
@@ -85,12 +86,69 @@ public class ELine extends AbstractEntity {
 
 	/**
 	 * Returns the departure times for this Line. These times are represented by
-	 * {@link ETimetable} objects.
+	 * {@link ETimestamp} objects.
 	 *
 	 * @return an immutable list containing this Line's departure times
 	 */
-	public List<ETimetable> getTimetables() {
+	public List<ETimestamp> getTimetables() {
 		return startTimes;
+	}
+
+	/**
+	 * Returns the distance from the first Station of this Line to another Station.
+	 *
+	 * @param station the Station to which to calculate the distance
+	 *
+	 * @return the total distance, or {@code -1} if this line does not pass from the
+	 *         given Station
+	 */
+	public double getDistanceToStation(EStation station) {
+
+		if (!stations.contains(station))
+			return -1;
+
+		if ((stations.size() == 1) || station.equals(stations.get(0)))
+			return 0;
+
+		double totalDistance = 0;
+
+		final Iterator<EStation> iter = stations.iterator();
+
+		EStation prev = iter.next();
+
+		for (EStation curr = iter.next();; prev = curr, curr = iter.next()) {
+			totalDistance += curr.getPosition().distanceFrom(prev.getPosition());
+
+			if (curr.equals(station))
+				break;
+		}
+
+		return totalDistance;
+	}
+
+	/**
+	 * Returns the next arrival time for this Line for a given Station.
+	 *
+	 * @param station   the Station for which to find the next arrival time
+	 * @param startTime the time after which to search for arrivals
+	 *
+	 * @return the next arrival time or {@code null} if no such time exists
+	 */
+	public ETimestamp getNextArrival(EStation station, ETimestamp startTime) {
+		final double distanceToStation = getDistanceToStation(station);
+		if (distanceToStation == -1)
+			return null;
+
+		final double     timeToStation = (distanceToStation / type.getAverageSpeed());
+		final ETimestamp travelTime    = ETimestamp.fromHours(timeToStation);
+
+		for (final Iterator<ETimestamp> iter = startTimes.iterator(); iter.hasNext();) {
+			final ETimestamp arrivalTime = iter.next().add(travelTime);
+			if (arrivalTime.compareTo(startTime) > 0)
+				return arrivalTime;
+		}
+
+		return null;
 	}
 
 	@Override
--- test/entity/ELineTest.java
+++ test/entity/ELineTest.java
@@ -2,7 +2,8 @@ package entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,23 +13,54 @@ import org.junit.jupiter.api.Test;
  *
  * @author Alex Mandelias
  */
+@SuppressWarnings("nls")
 class ELineTest {
 
 	private ELine defaultLine;
+	private List<EStation>   stations;
+	private List<ETimestamp> timetables;
 
-	/** Sets up a defaultTime object that will be used to test getter methods */
+	/** Sets up a defaultLine object that will be used to test getter methods */
 	@BeforeEach
 	void setUp() {
-		defaultLine = new ELine(-1, "A8", LineType.BUS, "marousi-akadhmia", new ArrayList<>(), //$NON-NLS-1$ //$NON-NLS-2$
-		        new ArrayList<>());
+		final ETown a = new ETown(-1, "Town A");
+		final ETown b = new ETown(-2, "Town B");
+
+		stations = new LinkedList<>();
+		stations.add(new EStation(-1, "Station A", new Position(1, 1), a));
+		stations.add(new EStation(-2, "Station B", new Position(1, 4), a));
+		stations.add(new EStation(-3, "Station C", new Position(5, 4), b));
+		stations.add(new EStation(-4, "Station D", new Position(9, 7), b));
+		stations.add(new EStation(-5, "Station E", new Position(4, 19), b));
+
+		timetables = new LinkedList<>();
+		timetables.add(new ETimestamp(10, 0));
+		timetables.add(new ETimestamp(10, 30));
+		timetables.add(new ETimestamp(11, 0));
+		timetables.add(new ETimestamp(11, 30));
+		timetables.add(new ETimestamp(12, 0));
+
+		defaultLine = new ELine(-1, "A8", LineType.BUS, "marousi-akadhmia", stations, timetables);
+	}
+
+	/**
+	 * Test method for
+	 * {@link entity.ELine#ELine(int, String, LineType, String, List, List)}.
+	 */
+	@SuppressWarnings("static-method")
+	@Test
+	final void testGetELine() {
+		final ELine emptyLine = new ELine(-1, null, null, null, null, null);
+		assertEquals(List.of(), emptyLine.getStations());
+		assertEquals(List.of(), emptyLine.getTimetables());
 	}
 
 	/**
 	 * Test method for {@link entity.ELine#getName()}.
 	 */
 	@Test
-	final void getLineNumber() {
-		assertEquals("A8", defaultLine.getName()); //$NON-NLS-1$
+	final void testGetLineNumber() {
+		assertEquals("A8", defaultLine.getName());
 	}
 
 	/**
@@ -44,7 +76,7 @@ class ELineTest {
 	 */
 	@Test
 	final void testGetName() {
-		assertEquals("marousi-akadhmia", defaultLine.getDescription()); //$NON-NLS-1$
+		assertEquals("marousi-akadhmia", defaultLine.getDescription());
 	}
 
 	/**
@@ -52,7 +84,7 @@ class ELineTest {
 	 */
 	@Test
 	final void testGetStations() {
-		assertEquals(new ArrayList<>(), defaultLine.getStations());
+		assertEquals(stations, defaultLine.getStations());
 	}
 
 	/**
@@ -60,6 +92,56 @@ class ELineTest {
 	 */
 	@Test
 	final void testGetTimeTables() {
-		assertEquals(new ArrayList<>(), defaultLine.getTimetables());
+		assertEquals(timetables, defaultLine.getTimetables());
+	}
+
+	/**
+	 * Test method for {@link entity.ELine#getDistanceToStation(EStation)}.
+	 */
+	@Test
+	final void testGetDistanceToStation() {
+		assertEquals(0.0, defaultLine.getDistanceToStation(stations.get(0)));
+		assertEquals(3.0, defaultLine.getDistanceToStation(stations.get(1)));
+		assertEquals(7.0, defaultLine.getDistanceToStation(stations.get(2)));
+		assertEquals(12.0, defaultLine.getDistanceToStation(stations.get(3)));
+		assertEquals(25.0, defaultLine.getDistanceToStation(stations.get(4)));
+
+		assertEquals(-1, defaultLine.getDistanceToStation(new EStation(-1, null, null, null)));
+
+		ELine oneStationLine = new ELine(-2, null, null, null, List.of(stations.get(1)) , null);
+		assertEquals(0.0, oneStationLine.getDistanceToStation(stations.get(1)));
+	}
+
+	/**
+	 * Test method for
+	 * {@link entity.ELine#getNextArrival(EStation, ETimestamp)}.
+	 */
+	@Test
+	final void testGetNextArrival() {
+
+		final ETimestamp start = new ETimestamp(10, 45);
+		final double speed = defaultLine.getType().getAverageSpeed();
+
+		ETimestamp arrivalAt0 = timetables.get(2);
+		assertEquals(arrivalAt0, defaultLine.getNextArrival(stations.get(0), start));
+
+		ETimestamp arrivalAt1 = timetables.get(2)
+		        .add(ETimestamp.fromMinutes((int) ((3.0 / speed) * 60)));
+		assertEquals(arrivalAt1, defaultLine.getNextArrival(stations.get(1), start));
+
+		ETimestamp arrivalAt2 = timetables.get(1)
+		        .add(ETimestamp.fromMinutes((int) ((7.0 / speed) * 60)));
+		assertEquals(arrivalAt2, defaultLine.getNextArrival(stations.get(2), start));
+
+		ETimestamp arrivalAt3 = timetables.get(1)
+		        .add(ETimestamp.fromMinutes((int) ((12.0 / speed) * 60)));
+		assertEquals(arrivalAt3, defaultLine.getNextArrival(stations.get(3), start));
+
+		ETimestamp arrivalAt4 = timetables.get(0)
+		        .add(ETimestamp.fromMinutes((int) ((25.0 / speed) * 60)));
+		assertEquals(arrivalAt4, defaultLine.getNextArrival(stations.get(4), start));
+
+		assertEquals(null, defaultLine.getNextArrival(stations.get(4), new ETimestamp(13, 30)));
+		assertEquals(null, defaultLine.getNextArrival(new EStation(-1, null, null, null), null));
 	}
 }
